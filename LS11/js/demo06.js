885be42d00f807bbc977332134c938425eacfa1e
chenzili <646582284@qq.com>
2018-04-11 21:36:43 +0800

    1

--- LS11/js/demo06.js
+++ LS11/js/demo06.js
@@ -3,27 +3,33 @@
  */
 
 //立即执行表达式 常见形式
-(function max( x,y){
+(function max(x,y){
     console.log("the max is",x>y?x:y);
 }(2,3));
+//3
 
 (function (x,y){ //可以没有函数名
     console.log("the min is",x<y?x:y);
 })(2,3);
+//2
 
 // 其他形式的IIFE 与运算符结合的写法
 var i = function(){
     return 10;
 }(); //i为10
-
+//10
 
 true && function(a,b){
     return a>b?a:b;
 }(5,9);
+//9
 
 
 !function(x,y){
     return x==y?true:false; // === 返回什么
 }("5",5);
+//false
 
 //思考 !function(){return 2; }( ); 与 !function(){return 0; }();
+//false
+//true
